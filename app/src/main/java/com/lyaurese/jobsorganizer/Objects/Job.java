b404e687e7b2e981e13559be11a83d1ec3ff44f4
lyaure <49193419+lyaure@users.noreply.github.com>
2021-11-03 22:30:45 +0200

    Create job object

---
+++ app/src/main/java/com/lyaurese/jobsorganizer/Objects/Job.java
@@ -0,0 +1,47 @@
+package com.lyaurese.jobsorganizer.Objects;
+
+public class Job {
+    String jobTitle;
+    String jobName;
+    String companyName;
+    boolean isCVSent;
+
+    public Job(String jobTitle, String jobName, String companyName, boolean isCVSent){
+        this.jobTitle = jobTitle;
+        this.jobName = jobName;
+        this.companyName = companyName;
+        this.isCVSent = isCVSent;
+    }
+
+    public String getJobTitle(){
+        return this.jobTitle;
+    }
+
+    public String getJobName(){
+        return this.jobName;
+    }
+
+    public String getCompanyName(){
+        return this.companyName;
+    }
+
+    public boolean isCVSent(){
+        return isCVSent;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public void setCVSent(boolean CVSent) {
+        isCVSent = CVSent;
+    }
+}
