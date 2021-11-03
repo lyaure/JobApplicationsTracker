89e0935a202a549f5a8cff157f892579f93d290f
lyaure <49193419+lyaure@users.noreply.github.com>
2021-11-03 22:30:06 +0200

    added bottom navigation bar

--- app/src/main/java/com/lyaurese/jobsorganizer/Activities/MainBoardActivity.java
+++ app/src/main/java/com/lyaurese/jobsorganizer/Activities/MainBoardActivity.java
@@ -1,16 +1,60 @@
 package com.lyaurese.jobsorganizer.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+import com.lyaurese.jobsorganizer.Fragments.BoardFragment;
+import com.lyaurese.jobsorganizer.Fragments.JobsFragment;
 import com.lyaurese.jobsorganizer.R;
 
-public class MainBoardActivity extends AppCompatActivity {
+public class MainBoardActivity extends FragmentActivity {
+    private BottomNavigationView bottomNavigation;
+    private Fragment fragment;
+    private int fragmentID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_board);
+
+        // bottom nav
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.setOnItemSelectedListener(navigationItemSelectedListener);
+
+        fragment = new BoardFragment();
+        loadFragment(fragment);
     }
+
+    // load fragment
+    private void loadFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container_ID, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+    private NavigationBarView.OnItemSelectedListener navigationItemSelectedListener =
+            new NavigationBarView.OnItemSelectedListener() {
+                @Override public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.nav_board:
+                            fragment = new BoardFragment();
+                            loadFragment(fragment);
+                            return true;
+                        case R.id.nav_jobs:
+                            fragment = new JobsFragment();
+                            loadFragment(fragment);
+                            return true;
+                    }
+                    return false;
+                }
+            };
 }
