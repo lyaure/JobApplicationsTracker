0897c9f68ae653b564e8c11d3d1c9c3c19c3cc19
lyaure <49193419+lyaure@users.noreply.github.com>
2021-11-22 13:33:45 +0200

    fix date prints

--- app/src/main/java/com/lyaurese/jobsorganizer/Utils/DateUtil.java
+++ app/src/main/java/com/lyaurese/jobsorganizer/Utils/DateUtil.java
@@ -8,6 +8,6 @@ public abstract class DateUtil {
     @SuppressLint("DefaultLocale")
     public static String getDate(Calendar calendar){
         String date = "%d/%d/%d";
-        return String.format(date, calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.MONTH) == 0 ? 12 : calendar.get(Calendar.MONTH), calendar.get(Calendar.YEAR));
+        return String.format(date, calendar.get(Calendar.DAY_OF_MONTH), calendar.get(Calendar.MONTH) == 0 ? 12 : calendar.get(Calendar.MONTH)+1, calendar.get(Calendar.YEAR));
     }
 }
